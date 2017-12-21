0f35a1b01a733a4b4f959bedec8db6cbcea64363
Hanna Sanina <hanna.sanina@thomsonreuters.com>
2017-12-21 13:35:18 +0300

    Task 5: Change Detection https://docs.google.com/document/d/1zCmethbiIxwnIyIvwMuHUxHbpW3OAeWhBnioL1SAwWI/edit

--- src/app/courses-page/course-item/course-item.component.ts
+++ src/app/courses-page/course-item/course-item.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Course } from './course';
-import { BorderHighlightDirective } from '../border-highlight.directive'
+import { BorderHighlightDirective } from '../border-highlight.directive';
+import { ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-course-item',
   templateUrl: './course-item.component.html',
   styleUrls: ['./course-item.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CourseItemComponent implements OnInit {
   @Input() course: Course;
   @Output() onDelete = new EventEmitter<Course>();
+  @Output() onEdit = new EventEmitter<Course>();
   showDialog = false;
 
   constructor() { }
@@ -20,6 +23,10 @@ export class CourseItemComponent implements OnInit {
   delete(course: Course) {
     this.onDelete.emit(course);
     this.showDialog = false;
+  }
+
+  edit(course: Course) {
+    this.onEdit.emit(course);
   }
 
 }
