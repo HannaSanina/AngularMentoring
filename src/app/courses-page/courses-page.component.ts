f81d1aa76217d322022a090ea81b77018943a2be
Hanna Sanina <hanna.sanina@thomsonreuters.com>
2017-12-28 14:03:38 +0300

    Module 6: RxJS
    https://docs.google.com/document/d/1NlryDrMJ990apEVOOb_fvUwZ7WIr23v0Gh0v-EtlKLw/edit

--- src/app/courses-page/courses-page.component.ts
+++ src/app/courses-page/courses-page.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from './course-item/course';
 import { CourseService } from '../services/course.service';
+import { Observable } from 'rxjs/Observable';
+import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
+import * as moment from 'moment';
+import { Subscription } from 'rxjs/Subscription';
+import { catchError, tap, map } from 'rxjs/operators';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/observable/from';
 
 @Component({
   selector: 'app-courses-page',
@@ -8,10 +15,11 @@ import { CourseService } from '../services/course.service';
   styleUrls: ['./courses-page.component.css']
 })
 
-export class CoursesPageComponent implements OnInit {
+export class CoursesPageComponent implements OnInit, OnDestroy {
   courseList: Course[];
   error: any;
   transform: any;
+  sub: Subscription;
 
   constructor(private courseService: CourseService) { }
 
@@ -19,26 +27,21 @@ export class CoursesPageComponent implements OnInit {
     this.getCourses();
   }
 
-  ngOnChanges() {
-    console.dir("OnChanges form Course component")
-  }
-
-  ngDoCheck() {
-    console.dir("DoCheck event form Course component")
-  }
-  ngAfterContentInit() {
-    console.dir("AfterContentInit event form Course component")
-  }
-  ngAfterContentChecked() {
-    console.dir("AfterContentChecked event form Course component")
-  }
-  ngAfterViewInit() {
-    console.dir("AfterViewInit event form Course component")
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 
   getCourses(): void {
-    this.courseService.getCourses()
-      .subscribe(items => this.courseList = items, error => { this.error = error.message; console.log(error); });
+    const millisecInDay = 1000 * 60 * 60 * 24;
+
+    this.sub = this.courseService.getCourses()
+      .map(v => v = v.filter(item => moment(item.createdDate).diff(moment(new Date())) > -14 * 1000 * 60 * 60 * 24))
+      .subscribe(items => {
+        console.dir(items);
+        this.courseList = items;
+      }, error => {
+        this.error = error.message; console.log(error);
+      });
   }
 
   add(newCourse: Course): void {
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,43 +1,52 @@
 import { Injectable } from '@angular/core';
 import { Headers, Response } from '@angular/http';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import 'rxjs/add/operator/map'
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
 import { User } from './user';
 import { stringify } from '@angular/compiler/src/util';
+import { ReplaySubject } from 'rxjs/ReplaySubject';
 
 @Injectable()
 export class AuthService {
-  public token: string;
+  token: string;
+  public isLogin = new ReplaySubject<boolean>(1);
 
   constructor(private http: HttpClient) {
-    let currentUser = JSON.parse(localStorage.getItem('currentUser'));
-    this.token = currentUser && currentUser.token;
+  //  let currentUser = JSON.parse(localStorage.getItem('currentUser'));
+//    this.token = currentUser && currentUser.token;
   }
 
-  login(username: string, password: string): Observable<boolean> {
-    return this.http.post('/api/authenticate', JSON.stringify({ username: username, password: password }))
-      .map((response: Response) => {
-        // login successful if there's a jwt token in the response
-        let token = response.json() && response.json().token;
+  login(username: string, password: string): void {
+    localStorage.setItem('currentUser', JSON.stringify({ user: "username", pass: "password" }));
+    this.isLogin.next(true);
+  //  this.http.post('/api/authenticate', JSON.stringify({ username: username, password: password }))
+/*    .map((response: Response) => {
+        const token = response.json() && response.json().token;
         if (token) {
           this.token = token;
-
+          this.isLogin.next(true);
           localStorage.setItem('currentUser', JSON.stringify({ username: username, token: token }));
-          return true;
         } else {
-          return false;
+          this.isLogin.next(false);
         }
-      });
+      }, error => {
+        console.dir(error);
+      }); */
   }
 
   logout(): void {
     this.token = null;
+    this.isLogin.next(false);
     localStorage.removeItem('currentUser');
   }
 
-  isAuth(): boolean {
-    return localStorage.getItem('currentUser') != undefined;
+  hasToken(): boolean {
+    return !!localStorage.getItem('currentUser');
+  }
+
+  isLoggedIn(): Observable<boolean> {
+    return this.isLogin.asObservable();
   }
 
   getUserInfo(): string {
